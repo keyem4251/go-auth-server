1a5e5c5701f3b52d22652110300b660a8be6f0af
keyem4251 <kisk.emura@gmail.com>
2024-11-04 20:44:55 +0900

    impl jwt

--- auth/token.go
+++ auth/token.go
@@ -203,22 +203,6 @@ func getCodeChallenge(codeVerifier string, codeChallengeMethod string) string {
 	return codeChallenge
 }
 
-// func (th *TokenHandler) createToken(clientId string, isRefresh bool) (string, string, error) {
-// 	buf := bytes.NewBufferString(clientId)
-// 	now := time.Now()
-// 	buf.WriteString(strconv.FormatInt(now.UnixNano(), 10))
-
-// 	// jwtではなくmd5
-// 	access := base64.URLEncoding.EncodeToString([]byte(uuid.NewMD5(uuid.Must(uuid.NewRandom()), buf.Bytes()).String()))
-// 	access = strings.ToUpper(strings.TrimRight(access, "="))
-// 	refresh := ""
-// 	if isRefresh {
-// 		refresh = base64.URLEncoding.EncodeToString([]byte(uuid.NewSHA1(uuid.Must(uuid.NewRandom()), buf.Bytes()).String()))
-// 		refresh = strings.ToUpper(strings.TrimRight(refresh, "="))
-// 	}
-// 	return access, refresh, nil
-// }
-
 func (th *TokenHandler) createJwtToken(clientId string, isRefresh bool) (string, string, error, error) {
 	accessClaims := jwt.RegisteredClaims{
 		Issuer:    "issuer",                                             // トークン発行者の識別子: URI形式（"https://example.us.auth0.com"）
