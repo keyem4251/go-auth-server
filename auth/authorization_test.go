23fe1fd92006d4442c27eaa5a1e32b8b96f1278a
keyem4251 <kisk.emura@gmail.com>
2024-11-05 21:02:17 +0900

    add test

--- auth/authorization_test.go
+++ auth/authorization_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -10,10 +11,18 @@ func TestHandleAuthorizeRequest(t *testing.T) {
 	db := NewDB()
 	ah := NewAuthorizeHandler(db)
 
-	req := httptest.NewRequest(http.MethodGet, "/authorize", nil)
+	endpoint := "/authorize"
+	responseType := "response_type=code"
+	clientId := "client_id=" + os.Getenv("CLIENT_ID")
+	redirectUri := "redirect_uri=" + os.Getenv("REDIRECT_URI")
+	state := "state=xyz"
+	codeChallenge := "code_challenge=123111111111111111111111111111111111111111111111111111111111"
+	codeChallengeMethod := "code_challenge_method=plain"
+	getParameter := "?" + responseType + "&" + clientId + "&" + redirectUri + "&" + state + "&" + codeChallenge + "&" + codeChallengeMethod
+	req := httptest.NewRequest(http.MethodGet, endpoint+getParameter, nil)
 	w := httptest.NewRecorder()
 	ah.HandleAuthorizeRequest(w, req)
-	if w.Result().StatusCode != 200 {
-		t.Errorf("status code is not 200: %v\n", w.Result().StatusCode)
+	if w.Result().StatusCode != 302 {
+		t.Errorf("status code is not 302: %v\n", w.Result().StatusCode)
 	}
 }
