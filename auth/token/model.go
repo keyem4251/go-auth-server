8b0623f380bb13d19d837c809a430070ecb3956b
keyem4251 <kisk.emura@gmail.com>
2024-11-10 21:00:48 +0900

    modelに分割

---
+++ auth/token/model.go
@@ -0,0 +1,163 @@
+package token
+
+import (
+	"auth/authorization"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Token struct {
+	ClientId     string
+	AccessToken  string
+	TokenType    string
+	ExpiresIn    int
+	RefreshToken string
+}
+
+func NewToken(
+	clientId string,
+	isRefresh bool,
+) (*Token, error, error) {
+	accessClaims := jwt.RegisteredClaims{
+		Issuer:    "issuer",                                             // トークン発行者の識別子: URI形式（"https://example.us.auth0.com"）
+		Subject:   clientId,                                             // 認証の対象となるユーザのID（クライアントIDではなくユーザー: "auth0|...." auth0でのユーザーIDとか）
+		Audience:  []string{clientId},                                   // トークンを利用する対象（APIなど: "http://localhost:8080"とか）
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(10 + time.Minute)), // トークンの有効期限
+		NotBefore: jwt.NewNumericDate(time.Now()),                       // トークンが有効となる日時
+		IssuedAt:  jwt.NewNumericDate(time.Now()),                       // トークンの発行日時
+		ID:        "id",                                                 // JWT の一意の ID
+	}
+	access := jwt.NewWithClaims(jwt.SigningMethodES256, accessClaims)
+	priv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	accessTokenString, accessTokenErr := access.SignedString(priv)
+	if accessTokenErr != nil {
+		log.Println("access token create error")
+		return nil, accessTokenErr, nil
+	}
+
+	refreshTokenString := ""
+	var refreshTokenErr error
+	if isRefresh {
+		RefreshTokenClaims := jwt.RegisteredClaims{
+			Issuer:    "issuer",
+			Subject:   "subject",
+			Audience:  []string{"audienct"},
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ID:        "id",
+		}
+		refresh := jwt.NewWithClaims(jwt.SigningMethodES256, RefreshTokenClaims)
+		refreshTokenString, refreshTokenErr = refresh.SignedString(priv)
+		if refreshTokenErr != nil {
+			log.Println("refresh token create error")
+			return nil, nil, refreshTokenErr
+		}
+	}
+	return &Token{
+		ClientId:     clientId,
+		AccessToken:  accessTokenString,
+		TokenType:    "Bearer",
+		RefreshToken: refreshTokenString,
+	}, nil, nil
+}
+
+type TokenRequest struct {
+	Method              string
+	GrantType           string
+	RedirectUri         string
+	AuthorizationHeader string
+	CodeVerifier        string
+}
+
+func NewTokenRequest(r *http.Request) *TokenRequest {
+	return &TokenRequest{
+		Method:              r.Method,
+		GrantType:           r.FormValue("grant_type"),
+		RedirectUri:         r.FormValue("redirect_uri"),
+		AuthorizationHeader: r.Header.Get("Authorization"),
+		CodeVerifier:        r.FormValue("code_verifier"),
+	}
+}
+
+func (tr *TokenRequest) validateTokenRequest() bool {
+	if tr.Method != "POST" {
+		log.Println("request method must be POST")
+		return false
+	}
+
+	if tr.GrantType != "authorization_code" {
+		log.Println("grant_type must be authorization_code")
+		return false
+	}
+
+	if tr.RedirectUri != os.Getenv("REDIRECT_URI") {
+		log.Println("redirect_uri is wrong")
+		return false
+	}
+
+	return true
+}
+
+func (tr *TokenRequest) parseAuthorizationHeader() (string, error) {
+	// 例）Authorization: Basic <BASE64エンコードしたユーザ名:パスワード>
+	if !strings.HasPrefix(tr.AuthorizationHeader, "Basic") {
+		return "", fmt.Errorf("authorization header is not basic")
+	}
+	basic := strings.Split(tr.AuthorizationHeader, "Basic ")[1]
+	decoded, err := base64.StdEncoding.DecodeString(basic)
+	if err != nil {
+		return "", err
+	}
+	clientId := strings.Split(string(decoded), ":")[0]
+	clientSecret := strings.Split(string(decoded), ":")[1]
+
+	if clientSecret != os.Getenv("CLIENT_SECRET") {
+		return "", fmt.Errorf("client secret is wrong")
+	}
+
+	return clientId, nil
+}
+
+func (tr *TokenRequest) validatePKCERequest(authorizationCode *authorization.AuthorizationCode, r *http.Request) bool {
+	if authorizationCode.CodeChallenge == nil && authorizationCode.CodeChallengeMethod == nil {
+		return true
+	}
+
+	if tr.CodeVerifier == "" {
+		log.Println("code_verifier is empty")
+		return false
+	}
+
+	// code_challengeでcode_verifierを変換して、保存されているcode_challengeと比較
+	codeChallenge := tr.getCodeChallenge(tr.CodeVerifier, *authorizationCode.CodeChallengeMethod)
+	if codeChallenge != *authorizationCode.CodeChallenge {
+		log.Println("code_verifier is wrong")
+		return false
+	}
+	return true
+}
+
+func (tr *TokenRequest) getCodeChallenge(codeVerifier string, codeChallengeMethod string) string {
+	if codeChallengeMethod == "plain" {
+		return codeVerifier
+	}
+
+	h := sha256.New()
+	h.Write([]byte(codeVerifier))
+	hashed := h.Sum(nil)
+
+	codeChallenge := base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(hashed)
+	return codeChallenge
+}
--- auth/token/token.go
+++ auth/token/token.go
@@ -1,45 +1,12 @@
 package token
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
-
-	"github.com/golang-jwt/jwt/v5"
 
 	"auth/authorization"
 )
-
-type Token struct {
-	ClientId     string
-	AccessToken  string
-	TokenType    string
-	ExpiresIn    int
-	RefreshToken string
-}
-
-func NewToken(
-	clientId string,
-	accessToken string,
-	tokenType string,
-	refreshToken string,
-) *Token {
-	return &Token{
-		ClientId:     clientId,
-		AccessToken:  accessToken,
-		TokenType:    tokenType,
-		RefreshToken: refreshToken,
-	}
-}
 
 type TokenHandler struct {
 	AuthRepo  *authorization.AuthorizationRepository
@@ -57,8 +24,9 @@ func NewTokenHandler(
 }
 
 func (th *TokenHandler) HandleTokenRequest(w http.ResponseWriter, r *http.Request) {
+	tr := NewTokenRequest(r)
 	// トークンリクエストを検証
-	if !th.validateTokenRequest(r) {
+	if !tr.validateTokenRequest() {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -66,15 +34,10 @@ func (th *TokenHandler) HandleTokenRequest(w http.ResponseWriter, r *http.Reques
 	// 認証ヘッダーを検証
 	// client secretを確認
 	// client idを取得して、データベースの値を取得
-	clientId, clientSecret, err := th.parseAuthorizationHeader(r)
+	clientId, err := tr.parseAuthorizationHeader()
 	if err != nil {
 		log.Printf("Error: %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if !th.validateClientSecret(clientSecret) {
-		w.WriteHeader(http.StatusForbidden)
 		return
 	}
 
@@ -86,7 +49,7 @@ func (th *TokenHandler) HandleTokenRequest(w http.ResponseWriter, r *http.Reques
 
 	// PKCEリクエストを検証
 	// client idから取得した値とcode challengeの値を作成して、検証
-	if !th.validatePKCERequest(authorizationCode, r) {
+	if !tr.validatePKCERequest(authorizationCode, r) {
 		w.WriteHeader(http.StatusForbidden)
 		return
 	}
@@ -95,7 +58,7 @@ func (th *TokenHandler) HandleTokenRequest(w http.ResponseWriter, r *http.Reques
 	// リフレッシュトークンはアクセストークンを発行するために必要
 	// 期限はアクセストークンが短い、リフレッシュトークンが長い
 	// アクセストークンの発行にはリフレッシュトークンと合わせてClientId,ClientSecretも必要
-	access, refresh, accessErr, refreshErr := th.createJwtToken(clientId, false)
+	tk, accessErr, refreshErr := NewToken(clientId, false)
 	if accessErr != nil {
 		log.Println(accessErr)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -107,11 +70,9 @@ func (th *TokenHandler) HandleTokenRequest(w http.ResponseWriter, r *http.Reques
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	tokenType := "Bearer"
 
 	// トークンを保存
-	token := NewToken(clientId, access, tokenType, refresh)
-	saveErr := th.TokenRepo.Save(token)
+	saveErr := th.TokenRepo.Save(tk)
 	if saveErr != nil {
 		log.Println("保存エラー")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -121,124 +82,8 @@ func (th *TokenHandler) HandleTokenRequest(w http.ResponseWriter, r *http.Reques
 	// レスポンスを作成
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]any{
-		"access_token":  access,
-		"token_type":    tokenType,
-		"refresh_token": refresh,
+		"access_token":  tk.AccessToken,
+		"token_type":    tk.TokenType,
+		"refresh_token": tk.RefreshToken,
 	})
-}
-
-func (th *TokenHandler) validateTokenRequest(r *http.Request) bool {
-	if r.Method != "POST" {
-		log.Println("request method must be POST")
-		return false
-	}
-
-	if r.FormValue("grant_type") != "authorization_code" {
-		log.Println("grant_type must be authorization_code")
-		return false
-	}
-
-	if r.FormValue("redirect_uri") != os.Getenv("REDIRECT_URI") {
-		log.Println("redirect_uri is wrong")
-		return false
-	}
-
-	return true
-}
-
-func (th *TokenHandler) parseAuthorizationHeader(r *http.Request) (string, string, error) {
-	// 例）Authorization: Basic <BASE64エンコードしたユーザ名:パスワード>
-	authorizationHeader := r.Header.Get("Authorization")
-	if !strings.HasPrefix(authorizationHeader, "Basic") {
-		return "", "", fmt.Errorf("authorization header is not basic")
-	}
-	basic := strings.Split(authorizationHeader, "Basic ")[1]
-	decoded, err := base64.StdEncoding.DecodeString(basic)
-	if err != nil {
-		return "", "", err
-	}
-	clientId := strings.Split(string(decoded), ":")[0]
-	clientSecret := strings.Split(string(decoded), ":")[1]
-	return clientId, clientSecret, nil
-}
-
-func (th *TokenHandler) validateClientSecret(clientSecret string) bool {
-	if clientSecret != os.Getenv("CLIENT_SECRET") {
-		log.Println("client secret is wrong")
-		return false
-	}
-	return true
-}
-
-func (th *TokenHandler) validatePKCERequest(authorizationCode *authorization.AuthorizationCode, r *http.Request) bool {
-	if authorizationCode.CodeChallenge == nil && authorizationCode.CodeChallengeMethod == nil {
-		return true
-	}
-
-	codeVerifier := r.FormValue("code_verifier")
-	if codeVerifier == "" {
-		log.Println("code_verifier is empty")
-		return false
-	}
-
-	// code_challengeでcode_verifierを変換して、保存されているcode_challengeと比較
-	codeChallenge := getCodeChallenge(codeVerifier, *authorizationCode.CodeChallengeMethod)
-	if codeChallenge != *authorizationCode.CodeChallenge {
-		log.Println("code_verifier is wrong")
-		return false
-	}
-	return true
-}
-
-func getCodeChallenge(codeVerifier string, codeChallengeMethod string) string {
-	if codeChallengeMethod == "plain" {
-		return codeVerifier
-	}
-
-	h := sha256.New()
-	h.Write([]byte(codeVerifier))
-	hashed := h.Sum(nil)
-
-	codeChallenge := base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(hashed)
-	return codeChallenge
-}
-
-func (th *TokenHandler) createJwtToken(clientId string, isRefresh bool) (string, string, error, error) {
-	accessClaims := jwt.RegisteredClaims{
-		Issuer:    "issuer",                                             // トークン発行者の識別子: URI形式（"https://example.us.auth0.com"）
-		Subject:   clientId,                                             // 認証の対象となるユーザのID（クライアントIDではなくユーザー: "auth0|...." auth0でのユーザーIDとか）
-		Audience:  []string{clientId},                                   // トークンを利用する対象（APIなど: "http://localhost:8080"とか）
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(10 + time.Minute)), // トークンの有効期限
-		NotBefore: jwt.NewNumericDate(time.Now()),                       // トークンが有効となる日時
-		IssuedAt:  jwt.NewNumericDate(time.Now()),                       // トークンの発行日時
-		ID:        "id",                                                 // JWT の一意の ID
-	}
-	access := jwt.NewWithClaims(jwt.SigningMethodES256, accessClaims)
-	priv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	accessTokenString, accessTokenErr := access.SignedString(priv)
-	if accessTokenErr != nil {
-		log.Println("access token create error")
-		return "", "", accessTokenErr, nil
-	}
-
-	refreshTokenString := ""
-	var refreshTokenErr error
-	if isRefresh {
-		RefreshTokenClaims := jwt.RegisteredClaims{
-			Issuer:    "issuer",
-			Subject:   "subject",
-			Audience:  []string{"audienct"},
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
-			NotBefore: jwt.NewNumericDate(time.Now()),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			ID:        "id",
-		}
-		refresh := jwt.NewWithClaims(jwt.SigningMethodES256, RefreshTokenClaims)
-		refreshTokenString, refreshTokenErr = refresh.SignedString(priv)
-		if refreshTokenErr != nil {
-			log.Println("refresh token create error")
-			return "", "", nil, refreshTokenErr
-		}
-	}
-	return accessTokenString, refreshTokenString, nil, nil
 }
